1422ab7c7f3cb2110c64eaba33eb916dd9f543de
Sri Aspari <mail@siarie.me>
2022-07-31 17:43:53 +0700

    lang(go): organize import on save
    Signed-off-by: Sri Aspari <mail@siarie.me>

--- lua/languages/go.lua
+++ lua/languages/go.lua
@@ -1,5 +1,6 @@
 -- [[ golang configuration ]]
 
+-- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim
 local lspconfig = require("lspconfig")
 local util = require("lspconfig.util")
 
@@ -16,3 +17,25 @@ lspconfig.gopls.setup({
         },
     },
 })
+
+vim.api.nvim_create_autocmd(
+    {"BufWritePre"},
+    {
+        pattern = {"*.go"},
+        callback = function()
+            local params = vim.lsp.util.make_range_params()
+            params.context = {only = {"source.organizeImports"}}
+            local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+            for _, res in pairs(result or {}) do
+                for _, r in pairs(res.result or {}) do
+                    if r.edit then
+                        vim.lsp.util.apply_workspace_edit(r.edit, "UTF-8")
+                    else
+                        vim.lsp.buf.execute_command(r.command)
+                    end
+                end
+            end
+        end
+    }
+)
+
