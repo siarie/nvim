e556885863b7103a64c0b2328712a7d2ba9a73b2
Sri Aspari <mail@siarie.me>
2023-12-11 13:49:05 +0700

    pm: change package manager to lazy vim

--- init.lua
+++ init.lua
@@ -1,16 +1,32 @@
 -- [[ init.lua ]]
 
 require("editor")
-require("ui")
+-- require("ui")
 require("keymaps")
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugin = require("plugin")
+require("lazy").setup(plugin)
+
 -- plugin configuration
-local packer_path = vim.fn.stdpath('config') .. '/site'
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path
+--local packer_path = vim.fn.stdpath('config') .. '/site'
+-- vim.o.packpath = vim.o.packpath .. ',' .. packer_path
 
-require("plugin")
+--require("plugin")
 
-require("tools")
+--require("tools")
 
 -- languages configuration
-require("languages")
+--require("languages")
--- lua/plugin.lua
+++ lua/plugin.lua
@@ -1,25 +1,26 @@
 -- [[ plugin.lua ]]
 
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+return {
+    --'wbthomason/packer.nvim'
 
-    -- [[ Appearance
-    use 'nvim-lualine/lualine.nvim'
-    use 'RRethy/nvim-base16'
-    use { 'NvChad/nvim-colorizer.lua', as = 'colorizer' }
+    --[[ Appearance ]]
+    { "nvim-lualine/lualine.nvim" },
+    { "RRethy/nvim-base16" },
+    -- { "NvChad/nvim-colorizer.lua" },
+    { "gpanders/editorconfig.nvim" },
 
-    -- [[ Tools ]]
+
+    --[[ Tool
     use 'ahmedkhalf/project.nvim'
     use 'neovim/nvim-lspconfig'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-    use 'gpanders/editorconfig.nvim'
-
-    use {
+        use {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
-end)
+    --]]
+}
